8446f58dc4a03fbd46f47a4dab685e473817738b
k1riesha <kiriesha999@gmail.com>
2024-01-09 21:47:11 +0200

    Add second commit

--- src/Homework04GitProgram.java
+++ src/Homework04GitProgram.java
@@ -2,5 +2,6 @@ public class Homework04GitProgram {
 
     public static void main(String[] args){
         System.out.println("Hello world from Kira to Git");
+        System.out.println("Second commit to Git");
     }
 }
